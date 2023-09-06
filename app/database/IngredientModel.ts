050c7b60becb21f2cdc5823143d070ea618482e1
Jun <junwen@outlook.com>
2023-09-06 03:15:32 +0000

    Created IngredientModel

---
+++ app/database/IngredientModel.ts
@@ -0,0 +1,33 @@
+// Get all ingredients
+// Add ingredient to user
+// Delete ingredient from user
+
+import { query } from "@/app/database/database";
+
+import { Results } from "@/app/database/types_database";
+
+export async function getAllIngredients() {
+  const sql = "select id, name, imagepath from ingredients";
+  return (await query(sql)) as Results;
+}
+
+export async function getUserIngredients(email: string) {
+  const sql = "select ingredient, imagepath from user_ingredients";
+  return (await query(sql, [email])) as Results;
+}
+
+export async function addUserIngredient(
+  email: string,
+  ingredient: string,
+  imagepath: string
+) {
+  const sql =
+    "insert into user_ingredients (email, ingredient, imagepath) values ($1, $2, $3)";
+  return (await query(sql, [email, ingredient, imagepath])) as Results;
+}
+
+export async function deleteUserIngredient(email: string, ingredient: string) {
+  const sql =
+    "delete from user_ingredients where email = $1 and ingredient = $2";
+  return (await query(sql, [email, ingredient])) as Results;
+}
